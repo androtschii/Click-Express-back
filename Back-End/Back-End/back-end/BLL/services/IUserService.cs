0fb5c0a83680057fdfc310767178460ba9a3d7f4
alasdooor <andrei_akbashlol@mail.ru>
2026-04-17 17:24:52 +0300

    Push 3: добавлен GET /api/User/profile для текущего пользователя

--- Back-End/Back-End/back-end/BLL/services/IUserService.cs
+++ Back-End/Back-End/back-end/BLL/services/IUserService.cs
@@ -1,11 +1,11 @@
 ﻿using back_end.BLL.DTOs;
-
 namespace back_end.BLL.Services
 {
     public interface IUserService
     {
         List<UserDto> GetAll();
         UserDto? GetById(int id);
+        UserDto? GetByUsername(string username);
         UserDto Create(CreateUserDto dto);
         UserDto? Update(int id, UpdateUserDto dto);
         bool Delete(int id);
