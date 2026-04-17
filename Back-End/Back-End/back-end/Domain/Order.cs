bfd2017b10897a8bc2a0494385405f2584d7d369
androtschii <bahov1006@gmail.com>
2026-04-17 14:29:20 +0300

    добавил Order, OrderItem, Cart, CartItem domain entities

---
+++ Back-End/Back-End/back-end/Domain/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace back_end.Domain
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Status { get; set; } = "Pending";
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
+        public User User { get; set; } = null!;
+        public List<OrderItem> Items { get; set; } = new();
+    }
+}
